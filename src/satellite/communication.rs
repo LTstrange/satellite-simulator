d06e267fe411ab7c380969af72133d5263c50d85
LTstrange <835422774@qq.com>
2024-10-31 13:09:49 +0800

    build ISL
    work for large scale
    Iterative, Greedy way.

--- src/satellite/communication.rs
+++ src/satellite/communication.rs
@@ -1,5 +1,3 @@
-use bevy::color::palettes::css::YELLOW;
-
 use crate::prelude::*;
 
 pub struct CommunicationPlugin;
@@ -20,8 +18,8 @@ struct Connections {
 
 #[derive(Event)]
 struct ConnectTwo {
-    a: Entity,
-    b: Entity,
+    from: Entity,
+    to: Entity,
 }
 
 fn setup(mut commands: Commands, satellites: Query<Entity, With<Satellite>>) {
@@ -32,38 +30,48 @@ fn setup(mut commands: Commands, satellites: Query<Entity, With<Satellite>>) {
     }
 }
 
-const CONNECTION_DIST: f32 = 2000.0;
-const CONNECTION_NUM: usize = 1;
+const CONNECTION_DIST: f32 = 6000.0;
+const CONNECTION_NUM: usize = 4;
 fn connect_nearest(
-    mut satellites: Query<(Entity, &Connections, &GlobalTransform), With<Satellite>>,
+    satellites: Query<(Entity, &Connections, &GlobalTransform), With<Satellite>>,
     mut connections: EventWriter<ConnectTwo>,
 ) {
-    let mut sat_pair = satellites.iter_combinations_mut();
-    let mut counter = 0;
-    while let Some([(sat1, conn1, trans1), (sat2, conn2, trans2)]) = sat_pair.fetch_next() {
-        if counter > 10 {
-            break;
-        }
-        if conn1.connections.len() < CONNECTION_NUM && conn2.connections.len() < CONNECTION_NUM {
-            let dist_sq = (trans1.translation() - trans2.translation()).length_squared();
+    let mut satellies_iter = satellites
+        .into_iter()
+        // filter out satellites that already saturate their connections
+        .filter(|(_, conn, _)| conn.connections.len() < CONNECTION_NUM)
+        // global transform to global coordinates
+        .map(|(sat, conn, trans)| (sat, conn, trans.translation()));
 
-            if dist_sq < CONNECTION_DIST * CONNECTION_DIST {
-                connections.send(ConnectTwo { a: sat1, b: sat2 });
-                counter += 1;
-            }
+    // find the first satellite and its connections
+    if let Some((cur_sat, cur_conn, cur_pos)) = satellies_iter.next() {
+        // get all other satellites within the connection distance
+        let mut other_satellites: Vec<_> = satellies_iter
+            .map(|(s, c, t)| (s, c, t.distance_squared(cur_pos)))
+            .filter(|(_, _, t)| *t < CONNECTION_DIST * CONNECTION_DIST)
+            .collect();
+        // sort by distance to the current satellite
+        other_satellites.sort_unstable_by(|a, b| a.2.total_cmp(&b.2));
+
+        let count = CONNECTION_NUM - cur_conn.connections.len();
+        for (other_sat, _, _) in &other_satellites[..count.min(other_satellites.len())] {
+            connections.send(ConnectTwo {
+                from: cur_sat,
+                to: *other_sat,
+            });
         }
     }
 }
 
 fn handle_connection(
-    mut satellites: Query<&mut Connections, With<Satellite>>,
+    mut satellites: Query<(Entity, &mut Connections), With<Satellite>>,
     mut connections: EventReader<ConnectTwo>,
 ) {
-    for ConnectTwo { a, b } in connections.read() {
-        let mut conn1 = satellites.get_mut(*a).unwrap();
-        conn1.connections.push(*b);
-        let mut conn2 = satellites.get_mut(*b).unwrap();
-        conn2.connections.push(*a);
+    for ConnectTwo { from, to } in connections.read() {
+        let mut from_conn = satellites.get_mut(*from).unwrap().1;
+        from_conn.connections.push(*to);
+        let mut to_conn = satellites.get_mut(*to).unwrap().1;
+        to_conn.connections.push(*from);
     }
 }
 
@@ -77,7 +85,31 @@ fn draw_connections(
         let start = global_trans.translation();
         for other_sat in &connections.connections {
             let end = satellites.get(*other_sat).unwrap().1.translation();
-            gizmos.arrow(start, end, YELLOW);
+            gizmos.arrow(
+                start,
+                end,
+                Srgba {
+                    red: 1.0,
+                    green: 1.0,
+                    blue: 0.0,
+                    alpha: 0.5,
+                },
+            );
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_iter() {
+        let sats = [1, 2, 3, 4, 5, 6];
+        let mut iter = sats.into_iter();
+        let first = iter.next().unwrap();
+        let rest = iter.collect::<Vec<_>>();
+
+        println!("first: {}, rest: {:?}", first, rest); // first: 1, rest: [2, 3, 4, 5, 6]
     }
 }
