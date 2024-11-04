68159c4f3616b90c7ec5b6156fd6a1fab3c7d63e
LTstrange <835422774@qq.com>
2024-11-04 23:20:30 +0800

    kinda know how to do...

--- src/control.rs
+++ src/control.rs
@@ -9,17 +9,14 @@ pub struct ControlPlugin;
 impl Plugin for ControlPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, (setup,));
-        app.add_systems(
-            FixedUpdate,
-            (handle_net_updates, tell_the_net_task_what_to_do),
-        );
+        app.add_systems(FixedUpdate, (handle_net_commands, handle_response_msg));
     }
 }
 
 #[derive(Resource)]
 struct MyNetChannel {
-    tx_control: Sender<NetControl>,
-    rx_reponse: Receiver<NetResponse>,
+    tx_response: Sender<NetResponse>,
+    rx_controls: Receiver<NetControl>,
 }
 
 /// Get control messages which change simulator's state
@@ -28,7 +25,11 @@ struct MyNetChannel {
 ///     Connect two pecific Sats.
 ///     Get all sats locations.
 ///     Make GroundStation send data through sats.
-enum NetControl {}
+enum NetControl {
+    StartRecordPos,
+    EndRecordPos,
+    GetPos,
+}
 
 /// Messages send out
 ///
@@ -36,37 +37,37 @@ enum NetControl {}
 enum NetResponse {}
 
 fn setup(mut commands: Commands, config: Res<Config>) {
-    let (tx_control, rx_control) = async_channel::unbounded();
+    let (tx_controls, rx_controls) = async_channel::unbounded();
     let (tx_response, rx_response) = async_channel::unbounded();
 
     let port = config.Network.port;
     IoTaskPool::get()
         .spawn(async move {
-            netcode(rx_control, tx_response, port).await;
+            netcode(tx_controls, rx_response, port).await;
         })
         .detach();
 
     commands.insert_resource(MyNetChannel {
-        tx_control,
-        rx_reponse: rx_response,
+        tx_response,
+        rx_controls,
     });
 }
 
-fn handle_net_updates(my_channels: Res<MyNetChannel>) {
+fn handle_net_commands(my_channels: Res<MyNetChannel>) {
     // Non-blocking check for any new messages on the channel
-    while let Ok(msg) = my_channels.rx_reponse.try_recv() {
+    while let Ok(msg) = my_channels.rx_controls.try_recv() {
         // TODO: do something with `msg`
     }
 }
 
-fn tell_the_net_task_what_to_do(my_channels: Res<MyNetChannel>) {
+fn handle_response_msg(my_channels: Res<MyNetChannel>) {
     // if let Err(e) = my_channels.tx_control.try_send(NetControl) {
     //     // TODO: handle errors. Maybe our task has
     //     // returned or panicked, and closed the channel?
     // }
 }
 
-async fn netcode(rx_control: Receiver<NetControl>, tx_response: Sender<NetResponse>, port: u16) {
+async fn netcode(tx_controls: Sender<NetControl>, rx_response: Receiver<NetResponse>, port: u16) {
     let listener = TcpListener::bind(("127.0.0.1", port)).await.unwrap();
     let mut incoming = listener.incoming();
 
