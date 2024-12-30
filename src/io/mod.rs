b69a04df8b91e989b2beecec3a3cca1e51d2edf3
LTstrange <835422774@qq.com>
2024-12-30 19:15:53 +0800

    use bevy brp

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,1 +1,11 @@
+use crate::prelude::*;
 
+use bevy::remote::{RemotePlugin, RemoteServer};
+
+pub struct IOPlugin;
+
+impl Plugin for IOPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((RemotePlugin,));
+    }
+}
