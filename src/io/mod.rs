5d346ba5b2eef51b61897e2a1e001f64209fe7c7
LTstrange <835422774@qq.com>
2025-01-05 00:29:22 +0800

    basic example for brp module

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,11 +1,17 @@
 use crate::prelude::*;
 
-use bevy::remote::{http::RemoteHttpPlugin, RemotePlugin};
+use bevy::remote::{http::RemoteHttpPlugin, BrpResult, RemotePlugin};
+use serde_json::Value;
 
 pub struct IOPlugin;
 
 impl Plugin for IOPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((RemoteHttpPlugin::default(), RemotePlugin::default()));
+        let remote_plugin = RemotePlugin::default().with_method("add_satellite", add_satellite);
+        app.add_plugins((RemoteHttpPlugin::default(), remote_plugin));
     }
+}
+
+fn add_satellite(param: In<Option<Value>>, mut commands: Commands) -> BrpResult<Value> {
+    todo!()
 }
