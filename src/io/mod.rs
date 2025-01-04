0b32e0d478fb000ddf57ee1362e029db5a3f53f0
LTstrange <835422774@qq.com>
2025-01-05 02:06:33 +0800

    add comments
    Added detailed comments to sort out how to write the function.

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -23,6 +23,33 @@ impl Plugin for IOPlugin {
     }
 }
 
+/// Add a satellite.
+///
+/// # Parameters
+/// - ID: String - The ID of the satellite.
+/// - orbitial elements: [Number, .. ] - The data of the satellite.
 fn add_satellite(param: In<Option<Value>>, mut commands: Commands) -> BrpResult<Value> {
+    // Example
+    // commands.spawn((
+    //     satellite,
+    //     Name::new(satellite_data.OBJECT_ID),
+    //     Mesh3d(satellite_mesh.clone()),
+    //     MeshMaterial3d(satellite_material.clone()),
+    //     Transform::from_translation(pos),
+    // ));
+
+    // What we need:
+    // - ID: String                         -- from parameter
+    // - orbitial elements: [Number, .. ]   -- from parameter
+    // - mesh: Mesh                         -- from a resource
+    // - material: StandardMaterial         -- from a resource
+
+    // commands.spawn((
+    //     Satellite {
+    //         // fill in satellite data
+    //     },
+    //     Name::new("satellite"),
+    //     // fill in satellite mesh and material
+    // ));
     todo!()
 }
