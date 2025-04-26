4c08835606d9be00e482270e2f1db1f4a65f160a
LTstrange <835422774@qq.com>
2025-04-26 15:27:44 +0800

    fix toggles later

--- simulator/src/ui/display_toggle.rs
+++ simulator/src/ui/display_toggle.rs
@@ -1,44 +1,15 @@
 use super::*;
 
-pub fn spawn_toggle(config: &Config) -> impl Bundle {
-    children![
-        (
-            Node {
-                align_items: AlignItems::Center,
-                column_gap: Val::Px(5.0),
-                ..default()
-            },
-            children![
-                widgets::Toggle(config.Display.orbit),
-                (Text::new("Show Orbit"), TextFont::from_font_size(18.0))
-            ],
-        ),
-        (
-            Node {
-                align_items: AlignItems::Center,
-                column_gap: Val::Px(5.0),
-                ..default()
-            },
-            children![
-                widgets::Toggle(config.Display.connection),
-                (Text::new("Show Connection"), TextFont::from_font_size(18.0))
-            ],
-        ),
-    ]
+pub fn toggle<T: Into<String>>(text: T, data: bool) -> impl Bundle {
+    (
+        Node {
+            align_items: AlignItems::Center,
+            column_gap: Val::Px(5.0),
+            ..default()
+        },
+        children![
+            widgets::Toggle(data),
+            (Text::new(text), TextFont::from_font_size(18.0))
+        ],
+    )
 }
-
-// fn toggle_orbit(
-//     trigger: Trigger<widgets::Activate>,
-//     toggle: Query<&widgets::Toggle>,
-//     mut config: ResMut<Config>,
-// ) {
-//     config.Display.orbit = toggle.get(trigger.entity()).unwrap().0;
-// }
-
-// fn toggle_connection(
-//     trigger: Trigger<widgets::Activate>,
-//     toggle: Query<&widgets::Toggle>,
-//     mut config: ResMut<Config>,
-// ) {
-//     config.Display.connection = toggle.get(trigger.entity()).unwrap().0;
-// }
--- simulator/src/ui/mod.rs
+++ simulator/src/ui/mod.rs
@@ -1,7 +1,5 @@
-use bevy::{
-    color::palettes::tailwind::{SKY_700, SLATE_50},
-    ecs::{relationship::RelatedSpawner, spawn::SpawnWith},
-};
+use bevy::ecs::{relationship::RelatedSpawner, spawn::SpawnWith};
+use display_toggle::toggle;
 use fps::fps;
 
 use crate::prelude::*;
@@ -14,13 +12,17 @@ pub struct UserInterfacePlugin;
 
 impl Plugin for UserInterfacePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(widgets::WidgetsPlugin);
         app.add_systems(Startup, setup);
         app.add_systems(Update, fps::fps_system);
     }
 }
 
 fn setup(mut commands: Commands, config: Res<Config>) {
+    let orbit = config.Display.orbit;
+    let connection = config.Display.connection;
+
+    info!("Orbit: {orbit}, Connection: {connection}");
+
     commands.spawn((
         Node {
             margin: UiRect::all(Val::Px(10.0)),
@@ -28,19 +30,28 @@ fn setup(mut commands: Commands, config: Res<Config>) {
             row_gap: Val::Px(10.),
             ..default()
         },
-        children![fps()],
+        children![
+            fps(),
+            // toggle("Show Orbit", orbit),
+            // toggle("Show Connection", connection)
+        ], // Children::spawn((
+           //     Spawn(fps()),
+           //     SpawnWith(move |parent: &mut RelatedSpawner<ChildOf>| {
+           //         parent.spawn(toggle("Show Orbit", orbit)).observe(
+           //             |trigger: Trigger<widgets::Activate>,
+           //              toggle: Query<&widgets::Toggle>,
+           //              mut config: ResMut<Config>| {
+           //                 config.Display.orbit = toggle.get(trigger.target()).unwrap().0;
+           //             },
+           //         );
+           //         parent.spawn(toggle("Show Connection", connection)).observe(
+           //             |trigger: Trigger<widgets::Activate>,
+           //              toggle: Query<&widgets::Toggle>,
+           //              mut config: ResMut<Config>| {
+           //                 config.Display.connection = toggle.get(trigger.target()).unwrap().0;
+           //             },
+           //         );
+           //     }),
+           // )),
     ));
-}
-
-fn button<T: Into<String>>(text: T) -> impl Bundle {
-    (
-        Button,
-        BackgroundColor(SKY_700.into()),
-        Node {
-            padding: UiRect::all(Val::Px(5.)),
-            width: Val::Px(200.),
-            ..default()
-        },
-        children![(Text::new(text), TextColor(SLATE_50.into()))],
-    )
 }
