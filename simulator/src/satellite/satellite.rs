3b0d3eeb6f274ef8ff64e32ad19701246d17f1fd
LTstrange <835422774@qq.com>
2025-04-27 20:21:05 +0800

    impl orbit sate relationship

--- simulator/src/satellite/satellite.rs
+++ simulator/src/satellite/satellite.rs
@@ -1,46 +1,71 @@
 use super::*;
 
 #[derive(Component)]
+#[require(Transform)]
 pub struct Satellite {
-    mean_anomaly: f32, // 平近点角(rad)
+    pub mean_anomaly: f32, // 平近点角(rad)
 }
 
-// pub fn update_mean_anomaly(
-//     config: Res<Config>,
-//     mut satellites: Query<&mut OrbitalElements, With<OrbitalElements>>,
-//     time: Res<Time<Fixed>>,
-// ) {
-//     for mut element in &mut satellites {
-//         element.mean_anomaly +=
-//             element.mean_motion * time.delta_secs() * config.Simulation.time_speed;
-//         element.mean_anomaly %= 2. * PI;
-//     }
-// }
+#[derive(Component, Debug)]
+#[relationship(relationship_target= FollowedBy)]
+pub struct Following(Entity);
 
-// pub fn update_satellite_position(mut satellites: Query<(&mut Transform, &OrbitalElements)>) {
-//     for (mut transform, orbital) in satellites.iter_mut() {
-//         transform.translation = get_position_from_orbital_elements(orbital);
-//     }
-// }
+#[derive(Component, Debug)]
+#[relationship_target(relationship= Following)]
+pub struct FollowedBy(Vec<Entity>);
 
-// pub fn get_position_from_orbital_elements(orbital: &OrbitalElements) -> Vec3 {
-//     let true_anomaly = anomaly_mean_to_true(orbital.mean_anomaly, orbital.eccentricity).unwrap();
-//     let n = orbital.mean_motion.powf(-2. / 3.);
-//     let semi_major_axis = FACTOR * n;
-//     // r = a(1- e^2) / (1 + e * cos(true_anomaly))
-//     let radius = semi_major_axis * (1.0 - orbital.eccentricity.powi(2))
-//         / (1. + orbital.eccentricity * true_anomaly.cos());
-//     let local_location = Vec3::new(
-//         radius * true_anomaly.cos(),
-//         radius * true_anomaly.sin(),
-//         0.0,
-//     ); // location on the orbital plane
+pub fn update_mean_anomaly(
+    config: Res<Config>,
+    orbits: Query<(&Orbit, &FollowedBy)>,
+    mut satellites: Query<&mut Satellite>,
+    time: Res<Time<Fixed>>,
+) -> Result {
+    for (orbit, sates) in orbits {
+        let mean_motion = orbit.mean_motion;
+        for sate in &sates.0 {
+            let mut sate = satellites.get_mut(*sate)?;
+            sate.mean_anomaly += mean_motion * time.delta_secs() * config.Simulation.time_speed;
+            sate.mean_anomaly %= 2. * PI;
+        }
+    }
 
-//     let rot = get_rotated_quat(
-//         orbital.inclination,
-//         orbital.longitude_of_ascending_node,
-//         orbital.argument_of_periapsis,
-//     );
+    Ok(())
+}
 
-//     rot * local_location // apply rotation
-// }
+pub fn update_satellite_position(
+    orbits: Query<(&Orbit, &FollowedBy)>,
+    mut satellites: Query<(&mut Transform, &Satellite)>,
+) -> Result {
+    for (orbit, sates) in orbits {
+        for sate in &sates.0 {
+            let (mut transform, sate) = satellites.get_mut(*sate)?;
+            transform.translation = get_position_from_orbital_elements(
+                &OrbitalElements::from_orbit_n_sate(orbit, sate),
+            );
+        }
+    }
+
+    Ok(())
+}
+
+pub fn get_position_from_orbital_elements(orbital: &OrbitalElements) -> Vec3 {
+    let true_anomaly = anomaly_mean_to_true(orbital.mean_anomaly, orbital.eccentricity).unwrap();
+    let n = orbital.mean_motion.powf(-2. / 3.);
+    let semi_major_axis = FACTOR * n;
+    // r = a(1- e^2) / (1 + e * cos(true_anomaly))
+    let radius = semi_major_axis * (1.0 - orbital.eccentricity.powi(2))
+        / (1. + orbital.eccentricity * true_anomaly.cos());
+    let local_location = Vec3::new(
+        radius * true_anomaly.cos(),
+        radius * true_anomaly.sin(),
+        0.0,
+    ); // location on the orbital plane
+
+    let rot = get_rotated_quat(
+        orbital.inclination,
+        orbital.longitude_of_ascending_node,
+        orbital.argument_of_periapsis,
+    );
+
+    rot * local_location // apply rotation
+}
