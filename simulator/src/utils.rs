deb5764e109293ab55459ce3b3df679d4f44385f
LTstrange <835422774@qq.com>
2025-04-30 19:17:39 +0800

    move raw sat data to config.rs

--- simulator/src/utils.rs
+++ simulator/src/utils.rs
@@ -2,46 +2,6 @@ use crate::prelude::*;
 
 use chrono::{DateTime, NaiveDateTime, Utc};
 
-/// Example of SatelliteData:
-/// {
-///     "OBJECT_NAME":"STARLINK-1008",
-///     "OBJECT_ID":"2019-074B",
-///     "EPOCH":"2024-10-27T04:10:58.101312",
-///     "MEAN_MOTION":15.06400535,
-///     "ECCENTRICITY":0.0001539,
-///     "INCLINATION":53.0535,
-///     "RA_OF_ASC_NODE":264.5418,
-///     "ARG_OF_PERICENTER":101.0513,
-///     "MEAN_ANOMALY":259.0649,
-///     "EPHEMERIS_TYPE":0,
-///     "CLASSIFICATION_TYPE":"U",
-///     "NORAD_CAT_ID":44714,
-///     "ELEMENT_SET_NO":999,
-///     "REV_AT_EPOCH":27361,
-///     "BSTAR":0.00030439,
-///     "MEAN_MOTION_DOT":4.255e-5,
-///     "MEAN_MOTION_DDOT":0
-/// }
-#[derive(Deserialize, Debug, Clone)]
-pub struct RawSatelliteData {
-    #[serde(rename = "OBJECT_ID")]
-    pub object_id: String,
-    #[serde(rename = "EPOCH")]
-    pub epoch: String,
-    #[serde(rename = "MEAN_MOTION")]
-    pub mean_motion: f32, // (rev/day)
-    #[serde(rename = "ECCENTRICITY")]
-    pub eccentricity: f32,
-    #[serde(rename = "INCLINATION")]
-    pub inclination: f32, // (degrees)
-    #[serde(rename = "RA_OF_ASC_NODE")]
-    pub ra_of_asc_node: f32, // (degrees)
-    #[serde(rename = "ARG_OF_PERICENTER")]
-    pub arg_of_pericenter: f32, // (degrees)
-    #[serde(rename = "MEAN_ANOMALY")]
-    pub mean_anomaly: f32, // (degrees)
-}
-
 // Copy from rastro crate (https://github.com/duncaneddy/rastro)
 pub fn anomaly_mean_to_true(anm_mean: f32, e: f32) -> Result<f32, String> {
     // Set constants of iteration
